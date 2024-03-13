80e17bb89d9f835c9a0b665dba6169012c7d96ee
Rui Menezes <ruizivo.menezes@gmail.com>
2024-03-13 10:57:51 -0300

    melhoria finish

--- scenes/finish.gd
+++ scenes/finish.gd
@@ -3,19 +3,19 @@ extends Area2D
 @export var target_level = PackedScene
 @onready var game_manager = %GameManager
 @onready var finish = %Finish
+@onready var collision_shape_2d = $CollisionShape2D
 
 
 func _on_body_entered(body):
-	if body.name == "CharacterBody2D" && finish.visible :
+	if body.name == "CharacterBody2D":
 		get_tree().change_scene_to_packed(target_level)
-
-
 	
 func OnPodeGanhar():
 	finish.visible = true
-
+	collision_shape_2d.set_deferred("disabled", false)
 
 func _on_ready():
+	collision_shape_2d.set_deferred("disabled", true)
 	game_manager.PodeGanhar.connect(OnPodeGanhar)
 	
 	
