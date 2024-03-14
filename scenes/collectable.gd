3ab288bd213371ecfba768da84ed9ea8e127dd4f
Rui Menezes <ruizivo.menezes@gmail.com>
2024-03-13 23:22:18 -0300

    adicionado musica de background e sons de pulo e coleta de itens

--- scenes/collectable.gd
+++ scenes/collectable.gd
@@ -1,9 +1,19 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
- 
+@onready var player = $coinFx
+@onready var collision = $CollisionShape2D
+@onready var anim = $AnimatedSprite2D
+
 
 func _on_body_entered(body):
 	if body.name == "CharacterBody2D":
-		queue_free()
+		anim.play("collect")
+		player.play()
 		game_manager.add_points()
+		collision.call_deferred("queue_free")
+
+
+func _on_animated_sprite_2d_animation_finished():  
+	if anim.animation == "collect":
+		queue_free()
--- scenes/main-character.gd
+++ scenes/main-character.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 400.0
 const JUMP_VELOCITY = -900.0
 @onready var sprite_2d = $Sprite2D
+@onready var player = $player
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -24,16 +25,19 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		sprite_2d.animation = "jumping"
+		
 			
 		if Input.is_action_just_pressed("jump") and not isInDoubleJump:
 			velocity.y = JUMP_VELOCITY
 			isInDoubleJump = true;
+			player.play()
 	else: 
 		isInDoubleJump = false;
 		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		player.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
